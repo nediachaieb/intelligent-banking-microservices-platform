eb5e43e37872d9ae6ffadbc52e4262acb33336a2
nediachaieb <nedia.chaieb.19@gmail.com>
2026-08-03 19:11:35 +0100

    chatService

--- chat-service/src/main/java/tn/nadia/chatservice/agents/AIAgent.java
+++ chat-service/src/main/java/tn/nadia/chatservice/agents/AIAgent.java
@@ -9,17 +9,33 @@ import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.util.Arrays;
+
 @Component
 public class AIAgent {
 
     private ChatClient chatClient;
 
     public AIAgent(ChatClient.Builder builder , ChatMemory memory , ToolCallbackProvider tools) {
+        Arrays.stream(tools.getToolCallbacks()).forEach(toolCallback -> {
+
+            System.out.println("--------------");
+            System.out.println(toolCallback.getToolDefinition());
+            System.out.println("--------------");
+
+
+        });
+
         this.chatClient = builder
                 .defaultSystem("""
-                        Vous un assistant qui se charge de répondre aux question
-                        de l'utilisateur en fonction du contexte fourni.
-                        Si aucun contexte n'est fourni, répond avec JE NE SAIS PAS         
+                         Vous êtes un assistant chargé de répondre aux questions
+                                de l'utilisateur à partir du contexte et des outils disponibles.
+                                                
+                                Utilisez les outils disponibles lorsque la question nécessite
+                                des données externes.
+                                                
+                                Si l'information demandée n'est disponible ni dans le contexte
+                                ni dans les outils, répondez uniquement : JE NE SAIS PAS.         
                         """)
                 .defaultAdvisors(
                         MessageChatMemoryAdvisor.builder(memory).build())
--- chat-service/src/main/java/tn/nadia/chatservice/web/ChatController.java
+++ chat-service/src/main/java/tn/nadia/chatservice/web/ChatController.java
@@ -1,12 +1,14 @@
 package tn.nadia.chatservice.web;
 
 
-import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 import tn.nadia.chatservice.agents.AIAgent;
+
+import java.awt.*;
 
 @RestController
 public class ChatController {
@@ -17,7 +19,7 @@ public class ChatController {
         this.aiAgent = aiAgent;
     }
 
-    @GetMapping("/chat")
+    @GetMapping(value = "/chat" ,produces = MediaType.TEXT_PLAIN_VALUE)
     public Flux<String> chat(@RequestParam String query) {
         return aiAgent.ask(query);
 
